cf75f460ea192b60c903065e662577d706e0e83b
NeverSettels <settels.eric@gmail.com>
2019-07-26 08:39:43 -0500

    done no styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import './App.css'
 
 function App() {
+  const [counter, setcounter] = useState(0)
+  const [left, setleft] = useState('')
+  const [right, setright] = useState('')
+  const [center, setcenter] = useState('')
+  function add() {
+    setcounter(prevstate => counter++)
+  }
+  function sub() {
+    setcounter(prevstate => counter--)
+  }
+  inputRightChange = e => {
+    const {
+      target: { name, value }
+    } = e
+    setright({
+      [name]: value
+    })
+  }
+  inputleftChange = e => {
+    const {
+      target: { name, value }
+    } = e
+    setleft({
+      [name]: value
+    })
+  }
+
+  inputcenterChange = e => {
+    const {
+      target: { name, value }
+    } = e
+    setcenter({
+      [name]: value
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{counter}</p>
+      <button onClick={add}>+</button>
+      <button onClick={sub}>-</button>
+      <div>
+        <input type="text" name="left" onChange={inputleftChange} />
+        <small>{left}</small>
+
+        <input type="text" name="center" onChange={inputcenterChange} />
+        <small>{center}</small>
+
+        <input type="text" name="right" onChange={inputRightChange} />
+        <small>{right}</small>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
